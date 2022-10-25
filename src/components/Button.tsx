24753a9b93f09295eacf25e56e4275933b6820d4
Anselmo <anselmo3.santos@gmail.com>
2022-10-25 12:02:11 -0300

    Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,9 +13,9 @@ export function Button({ children, asChild, className, ...props }: ButtonProps) 
   return (
     <Comp 
       className={clsx(
-        'py-3 px-4 bg-cyan-500'+
-        'rounded font-semibold text-black'+
-        'text-sm w-full transition-colors' +
+        'py-3 px-4 bg-cyan-500 '+
+        'rounded font-semibold text-black '+
+        'text-sm w-full transition-colors ' +
         'hover:bg-cyan-300 focus:ring-2 ring-white',
         className,
       )}
